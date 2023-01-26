ffd8349e382805354e2e88e816f26ff455061038
dparaiso <danieva.paraiso@gmail.com>
2023-01-25 23:29:56 -0800

    Ready for recording

--- Assets/StarterAssets/Editor/StarterAssetsDeployMenu.cs
+++ Assets/StarterAssets/Editor/StarterAssetsDeployMenu.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Linq;
-using StarterAssetsPackageChecker;
 using UnityEditor;
 using UnityEngine;
 using Object = UnityEngine.Object;
@@ -28,16 +27,6 @@ namespace StarterAssets
         private const string CinemachineTargetTag = "CinemachineTarget";
 
         private static GameObject _cinemachineVirtualCamera;
-        
-        /// <summary>
-        /// Deletes the scripting define set by the Package Checker.
-        /// See Assets/Editor/PackageChecker/PackageChecker.cs for more information
-        /// </summary>
-        [MenuItem(MenuRoot + "/Reinstall Dependencies", false)]
-        static void ResetPackageChecker()
-        {
-            PackageChecker.RemovePackageCheckerScriptingDefine();
-        }
 
 #if STARTER_ASSETS_PACKAGES_CHECKED
         private static void CheckCameras(Transform targetParent, string prefabFolder)
